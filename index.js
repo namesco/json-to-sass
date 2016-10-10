50d1b3e343201503d871416345fb5d81b2c21607
Jack Westbrook <jwestbrook@names.co.uk>
2016-10-10 14:57:20 +0100

    Added the ability to convert to camel case or to kebab case when running json to sass.

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 var _ = require('lodash');
 
-module.exports = function(data) {
+module.exports = function(data, options) {
 	var scss = '';
 
-    if (!data) {
-    	return;
-    }
-    
-    data = JSON.parse(data);
+	if (!data) {
+		return;
+	}
+	
+	data = JSON.parse(data);
 
-    _.mapKeys(data, function(value, key) {
+	function toCamelCase(str) {
+		return str.replace(/(?!^)[_|-]([a-z])/g, function(matches) {
+			return matches[1].toUpperCase();
+		});
+	}
+
+	function toKebab(str) {
+		return str.replace(/(?!^)[_A-Z]/g, function(matches) {
+			if(matches[0] == '_') {
+			  return '-';
+			}
+			return '-' + matches.toLowerCase();
+		});
+	}
+
+	_.mapKeys(data, function(value, key) {
+
+		if (options.convertTo) {
+
+			if (options.convertTo == 'camel') {
+				key = toCamelCase(key);
+			}
+
+			if (options.convertTo == 'kebab') {
+				key = toKebab(key);
+			}
+		}
+
 		scss+= '$' + key + ': ' + value + ';\r\n'
 	});
 
